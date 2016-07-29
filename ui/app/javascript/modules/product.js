9ac2ff90e3d9bd7b456ad04c3dc692ce8da710f2
Josh Parrett <jtparrett@me.com>
2016-07-29 13:09:19 +0100

    shopify conversion

--- ui/app/javascript/modules/product.js
+++ ui/app/javascript/modules/product.js
@@ -3,16 +3,35 @@ JP.product = function(){
   function init(){
     options.bigProduct = byBehaviour('big-product');
     byBehaviour('small-product').on('click', change);
+
+    new Shopify.OptionSelectors('product-select', { 
+      product: {{ product | json }}, 
+      onVariantSelected: selectCallback, 
+      enableHistoryState: true 
+    });
   }
 
   function change(){
-    console.log('pow');
     var $this = $(this);
     var src = $this.attr('src');
 
     $this.attr('src', options.bigProduct.attr('src'));
     options.bigProduct.attr('src', src);
     return false;
+  }
+
+  function selectCallback(variant, selector){
+    if(!variant){
+      return false;
+    }
+    
+    if(variant.available){
+      $('#add').removeClass('button--disabled').prop('disabled', false).val({{ add_to_cart | json }});    
+    } else {
+      $('#add').addClass('button--disabled').prop('disabled', true).val({{ sold_out | json }});
+    }
+    
+    $('#product-price').html(Shopify.formatMoney(variant.price, "{{ shop.money_format }}"));
   }
 
   return {
