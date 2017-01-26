600cf1fde2670aff8ab106a55f269ab065f6a6a5
Josh Parrett <jtparrett@me.com>
2017-01-26 15:42:50 +0000

    banner updates

--- src/javascript/data/carousel.jsx
+++ src/javascript/data/carousel.jsx
@@ -3,25 +3,25 @@ import React from 'react'
 import CarouselImage from '../components/carousel-image'
 import CarouselVideo from '../components/carousel-video'
 
-import 'images/IMG_2630.JPG'
-import 'images/IMG_2631.JPG'
-import 'images/IMG_1804.JPG'
-import 'images/IMG_3023.JPG'
-import 'images/IMG_6815.JPG'
-import 'images/IMG_1755.JPG'
-import 'images/IMG_8175.JPG'
+import 'images/banner-1.jpg'
+import 'images/banner-2.jpg'
+import 'images/banner-3.jpg'
+import 'images/banner-4.jpg'
+import 'images/banner-5.jpg'
+import 'images/banner-6.jpg'
+import 'images/banner-7.jpg'
 
 export function CarouselData() {
   return [() => {
-    return (<CarouselImage src={ ['/assets/images/IMG_2630.JPG', '/assets/images/IMG_2631.JPG'] } />)
+    return (<CarouselImage src={ ['/assets/images/banner-1.jpg', '/assets/images/banner-2.jpg'] } />)
   }, () => {
     return (<CarouselVideo src="https://www.facebook.com/JParreApparel/videos/593954704148136" />)
   }, () => {
-    return (<CarouselImage src={ ['/assets/images/IMG_1804.JPG', '/assets/images/IMG_3023.JPG', '/assets/images/IMG_6815.JPG'] } />)
+    return (<CarouselImage src={ ['/assets/images/banner-3.jpg', '/assets/images/banner-4.jpg', '/assets/images/banner-5.jpg'] } />)
   }, () => {
     return (<CarouselVideo src="https://www.facebook.com/JParreApparel/videos/593000240910249" />)
   }, () => {
-    return (<CarouselImage src={ ['/assets/images/IMG_8175.JPG', '/assets/images/IMG_1755.JPG'] } />)
+    return (<CarouselImage src={ ['/assets/images/banner-6.jpg', '/assets/images/banner-7.jpg'] } />)
   }, () => {
     return (<CarouselVideo src="https://www.facebook.com/JParreApparel/videos/566298463580427" />)
   }]
