cf1642a5cee9e841d1e0e37c7ca4735d65b585d4
jtparrett <jtparrett@me.com>
2017-05-03 19:21:29 +0100

    enlarge icon

--- src/javascript/components/product.jsx
+++ src/javascript/components/product.jsx
@@ -3,11 +3,16 @@ import { Link } from 'react-router'
 
 import 'stylesheets/components/product'
 
+import EnlargeIcon from '../../images/icon-enlarge.svg'
+
 export default class Product extends React.Component {
   render(){
-    let { product } = this.props
+    let { product, enlarge } = this.props
     return (
       <Link to={ `/products/${product.attrs.handle}` } onClick={ this.props.onClick } className="product">
+        { enlarge &&
+          <img src={ EnlargeIcon } className="product__icon" />
+        }
         <img src={ product.variants[0].imageVariants[this.props.imageSize].src } className="product__image" />
       </Link>
     )
--- src/javascript/views/products/show.jsx
+++ src/javascript/views/products/show.jsx
@@ -76,7 +76,7 @@ export default class ProductShow extends React.Component {
             }) } />
           </div>
           <section className="actions">
-            <button className="button" onClick={ this.closeModal }>Close</button>
+            <button className="button" onClick={ this.closeModal }>&larr; Back to Product</button>
           </section>
         </div>
       )
@@ -85,7 +85,7 @@ export default class ProductShow extends React.Component {
     return (
       <div>
         <section className="wrapper wrapper--slim">
-          <Product product={ products[0] } imageSize={7} onClick={ this.openModal }/>
+          <Product product={ products[0] } imageSize={7} onClick={ this.openModal } enlarge={true} />
           <article className="detail">
             <h1 className="detail__title">{ products[0].title }</h1>
             <div dangerouslySetInnerHTML={{ __html: products[0].attrs.body_html }} className="wysiwyg"></div>
