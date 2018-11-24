47389ba5fb9dc4b30c50e432525d2815a421d381
jtparrett <jtparrett@me.com>
2018-11-24 14:48:24 +0000

    checkout mutation

--- src/molecules/ProductForm/index.js
+++ src/molecules/ProductForm/index.js
@@ -1,22 +1,63 @@
 import React from 'react'
 import styled from 'styled-components'
+import {Mutation} from 'react-apollo'
+import gql from 'graphql-tag'
 
 import Button from '../../atoms/Button'
 import Select from '../../atoms/Select'
+import Loader from '../../atoms/Loader'
 
 const Form = styled.form`
   display: flex;
   padding: 0 20px;
 `
 
-export default ({variants}) => (
-  <Form>
-    <Select>
-      {variants.map(({node}) => (
-        <option key={node.id}>{node.title}</option>
-      ))}
-    </Select>
+const ADD_LINE_ITEM = gql`
+  mutation($input:CheckoutCreateInput!){
+    checkoutCreate(input:$input) {
+      checkout {
+       webUrl
+      }
+    }
+  }
+`
 
-    <Button type="submit">Purchase</Button>
-  </Form>
+export default ({variants}) => (
+  <Mutation mutation={ADD_LINE_ITEM}>
+    {(checkout, {error, loading, data}) => {
+      
+      if(error) return (<p>Error checking out</p>)
+
+      if(loading) return (<Loader />)
+
+      if(data){
+        window.location.replace(data.checkoutCreate.checkout.webUrl)
+        return (<Loader />)
+      }
+
+      return (
+        <Form onSubmit={(e) => {
+          e.preventDefault()
+          checkout({
+            variables: {
+              input: {
+                lineItems: [{
+                  quantity: 1,
+                  variantId: e.target.variant.value
+                }]
+              }
+            }
+          })
+        }}>
+          <Select name="variant">
+            {variants.map(({node}) => node.availableForSale && (
+              <option value={node.id} key={node.id}>{node.title}</option>
+            ))}
+          </Select>
+
+          <Button type="submit">Purchase</Button>
+        </Form>
+      )
+    }}
+  </Mutation>
 )
